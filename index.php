a40870c0bec130bd7a0e6184e6938ed84946ae89
fernandoperez3108 <fernandoperezfloresjr@gmail.com>
2023-12-07 16:51:34 -0400

    feat:progress task

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    include(("database/db.php");
+?>
+
+<?php
+    include("includes/header.php");
+?>
+
+<main class="container p-4 mt-5 mb-5">
+    <div class="row mt-5 mb-5">
+        <div class="col-md-4">
+            
+        </div>
+    </div>
+</main>
